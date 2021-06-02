56c56b223e6bd892e308008a402fc9f8c3edc58e
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-06-02 17:54:32 +0530

    Add files via upload

---
+++ Nine.js
@@ -0,0 +1,28 @@
+console.log(Math.max(4, 9) + 100);
+console.log(Math.min(4, 29) + 200);
+if (1 + 1 == 44)
+    console.log("It's true");
+else
+    console.log("Its not a true");
+let Number = 0;
+while (Number <= 12) {
+    console.log(Number);
+    Number = Number + 2;
+}
+let result = 1;
+let counter = 0;
+while (counter < 10) {
+    result = result * 2;
+    counter = counter + 1;
+
+}
+console.log(result);
+if (false != true) {
+    console.log("That makes sense.");
+    if (1 < 2) {
+        console.log("No surprise there.");
+    }
+}
+for (let Number = 0; Number <= 12; Number = Number + 12) {
+    console.log(Number);
+}
---
+++ Ten.js
@@ -0,0 +1,29 @@
+let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+    result = result * 2;
+}
+
+//result = result * 2;
+//]
+console.log(result);
+let name
+name = 12;
+var na;
+na = 23;
+console.log(name);
+console.log(na);
+help = 34;
+console.log(help);
+let gh;
+gh = 45;
+gh = 89;
+const pk = 89;
+// pk = 89;
+console.log(pk);
+console.log(gh);
+for (let current = 20; current = current + 1;) {
+    if (current % 7 == 0) {
+        console.log(current);
+        break;
+    }
+}
